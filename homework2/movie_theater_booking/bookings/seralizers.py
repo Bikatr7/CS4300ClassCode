085878d65a0e5ef460b67200dd44f2cfcaf89ee8
Kaden Bilyeu <Bikatr7@proton.me>
2025-02-18 10:16:52 -0700

    tests, urls, and some templates

--- homework2/movie_theater_booking/bookings/seralizers.py
+++ homework2/movie_theater_booking/bookings/seralizers.py
@@ -4,15 +4,14 @@ from .models import Movie, Seat, Booking
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id', 'title', 'description', 'release_date', 'duration']
+        fields = '__all__'
 
 class SeatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Seat
-        fields = ['id', 'seat_number', 'booking_status']
+        fields = '__all__'
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = ['id', 'movie', 'seat', 'user', 'booking_date']
-        read_only_fields = ['booking_date']  # booking_date will be set automatically
+        fields = '__all__'
--- homework2/movie_theater_booking/bookings/tests.py
+++ homework2/movie_theater_booking/bookings/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Movie
+from datetime import date
 
-# Create your tests here.
+class MovieTestCase(TestCase):
+    def setUp(self):
+        Movie.objects.create(
+            title="Test Movie",
+            description="A sample description",
+            duration=120,
+            release_date=date.today()
+        )
+
+    def test_movie_creation(self):
+        movie = Movie.objects.get(title="Test Movie")
+        self.assertEqual(movie.duration, 120)
